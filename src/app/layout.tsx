57fb37edc2c1db5339da7a81bd25deda408642e2
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2025-04-23 00:15:28 +0300

    V1 File Managment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,20 @@
 "use client"
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
-import "quill/dist/quill.snow.css";
 import { ThemeProvider } from "next-themes";
+import { ReactNode } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <ThemeProvider>
         <SessionProvider>
-            <body
-              className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-              {children}
+          <ThemeProvider>
+            <body className="min-h-screen bg-white dark:bg-[#383c4a] text-black dark:text-white">
+                {children}
             </body>
+          </ThemeProvider>
         </SessionProvider>
-    </ThemeProvider>
     </html>
   );
 }
